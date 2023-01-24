82429e2bdef64557f6328a25eee755f2d252bb08
ProgKea <keanu.daniel@e-mail.de>
2023-01-24 06:27:42 +0100

    rename emacs config file from init.el to emacs

--- install.ps1
+++ install.ps1
@@ -1,5 +1,5 @@
 winget install emacs
-$emacs_config_url="https://raw.githubusercontent.com/ProgKea/windows_config/master/init.el"
+$emacs_config_url="https://raw.githubusercontent.com/ProgKea/windows_config/master/emacs"
 curl.exe $emacs_config_url -o $env:APPDATA/.emacs
 winget install powertoys
 PowerToys.exe run KeyboardManager remap -from CapsLock -to Escape
