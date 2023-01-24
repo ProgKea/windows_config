1645e979c00b0498db1c3e6654410cec7794a0e6
ProgKea <keanu.daniel@e-mail.de>
2023-01-24 06:25:53 +0100

    add rebind command to install script

--- install.ps1
+++ install.ps1
@@ -2,3 +2,4 @@ winget install emacs
 $emacs_config_url="https://raw.githubusercontent.com/ProgKea/windows_config/master/init.el"
 curl.exe $emacs_config_url -o $env:APPDATA/.emacs
 winget install powertoys
+PowerToys.exe run KeyboardManager remap -from CapsLock -to Escape
